27742adba473bcc08d1a05f2b3cc9cc59c47883d
sakutars123 <contact.satyamkunal@gmail.com>
2022-01-25 23:07:11 +0530

    Initial commit

---
+++ src/main/java/com/demo/SpringBootMessaging/MessagingApplicationSpringBoot/MessagingApplicationSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.demo.SpringBootMessaging.MessagingApplicationSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagingApplicationSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessagingApplicationSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/SpringBootMessaging/MessagingApplicationSpringBoot/MessagingApplicationSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.SpringBootMessaging.MessagingApplicationSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessagingApplicationSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
